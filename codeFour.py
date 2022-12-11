c4396137b8ed95033a73f312c5b10eeb7e89b012
Gennadii <brundrom@gmail.com>
2022-12-11 11:31:10 +0300

    Castles for kings

---
+++ codeFour.py
@@ -0,0 +1,8 @@
+king = 'King Balon the 6th'
+
+# BEGIN (write your solution here)
+kings_count = 6
+rooms_in_kings_castle = 17
+rooms = kings_count * rooms_in_kings_castle
+print(king + ' has ' + str(rooms) + ' rooms.')
+# END
