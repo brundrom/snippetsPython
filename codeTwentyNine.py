26d1627f987b81d72de3e55d63c4ee8fb1ad0b7e
Gennadii <brundrom@gmail.com>
2022-12-14 22:10:42 +0300

    Check string for containing char

--- codeTwentyNine.py
+++ codeTwentyNine.py
@@ -9,10 +9,15 @@
 
 def is_contains_char(text, sym):
     i = 0
-    while i <= len(text):
-        return text[i] == sym.lower() or text[i] == sym.upper()
+    result = False
+    while i < len(text):
+        if text[i] == sym.lower() or text[i] == sym.upper():
+            result = True
+        i += 1
+    return result
 
 
 print(is_contains_char('Hexlet', 'H'))  # => True
 print(is_contains_char('Hexlet', 'h'))  # => True
 print(is_contains_char('Awesomeness', 'd'))  # => False
+print(is_contains_char('Awesomeness', 'm'))  # => True
