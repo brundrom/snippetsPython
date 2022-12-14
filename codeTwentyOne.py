b8916428af7a976b150d4bd61b5472a427addda7
Gennadii <brundrom@gmail.com>
2022-12-14 17:47:11 +0300

    Program for calculate age difference

---
+++ codeTwentyOne.py
@@ -0,0 +1,12 @@
+# Напишите функцию get_age_difference(), которая принимает два года рождения и возвращает строку с разницей в возрасте в виде The age difference is 11
+# actual = get_age_difference(2001, 2018)
+# print(actual)  # => The age difference is 17
+
+
+def get_age_difference(year_past, year_present):
+    result = year_present - year_past
+    return 'The age difference is ' + str(result)
+
+
+actual = get_age_difference(2001, 2018)
+print(actual)
