c78085b73b5ac41a4bde3fa171116f54210edb37
Gennadii <brundrom@gmail.com>
2022-12-14 15:15:10 +0300

    Abs with sum

---
+++ codeEleven.py
@@ -0,0 +1,9 @@
+# Напишите программу, которая использует функцию abs() и выводит на экран модуль суммы двух переменных num1 и num2.
+
+num1 = 10
+num2 = -13
+
+# BEGIN (write your solution here)
+abs_with_sum = abs(num1 + num2)
+print(str(abs_with_sum))
+# END
