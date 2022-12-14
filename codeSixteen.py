22b067e43da23cd4019c92dbf8be18e4d5a7fbfe
Gennadii <brundrom@gmail.com>
2022-12-14 16:09:18 +0300

    Task with slice, strip and length

---
+++ codeSixteen.py
@@ -0,0 +1,11 @@
+# Task:
+# С помощью слайсов, получите часть предложения, записанного в переменную text, c 5 по 16 символы включительно.
+# Полученную подстроку обработайте методом .strip() и выведите на экран длину итоговой подстроки.
+# Выполните эти операции подряд в цепочке без создания промежуточных переменных.
+
+text = 'When \t\n you play a \t\n game of thrones you win or you die.'
+
+# BEGIN (write your solution here)
+slice_params = slice(5, 16)
+print(len(text[slice_params].strip()))
+# END
