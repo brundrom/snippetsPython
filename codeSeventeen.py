b2c6205085b232bc56a0793a65275a84e0a23cea
Gennadii <brundrom@gmail.com>
2022-12-14 16:39:53 +0300

    function for printing text line

---
+++ codeSeventeen.py
@@ -0,0 +1,11 @@
+# BEGIN (write your solution here)
+# Call function
+
+def say_hurray_three_times():
+    text = 'hurray! hurray! hurray!'
+    return text
+
+
+result = say_hurray_three_times()
+print(result)  # => 'hurray! hurray! hurray!'
+# END
