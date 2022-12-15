b534300853d10bd6cfc7a1324d5b997af372b288
Gennadii <brundrom@gmail.com>
2022-12-15 19:01:39 +0300

    Program for check is palindrome or not

--- codeThirtyOne.py
+++ codeThirtyOne.py
@@ -1,13 +1,19 @@
-
+# Is palindrome?
 def is_palindrome(text):
     if text == "":
         return True
     else:
+        it_word = text.lower()
+        it_reversed_word = ''
+        word_length = len(it_word)
+        while word_length > 0:
+            it_reversed_word += it_word[word_length - 1]
+            word_length -= 1
+        return it_word == it_reversed_word
 
 
-
-is_palindrome('')  # True пустая строка тоже считается палиндромом
-is_palindrome('radar') # True
-is_palindrome('a') # True
-is_palindrome('abs') # False
-is_palindrome('ишак ищет у тещи каши') # True
+print(is_palindrome(''))  # True пустая строка тоже считается палиндромом
+print(is_palindrome('radar')) # True
+print(is_palindrome('a')) # True
+print(is_palindrome('abs')) # False
+print(is_palindrome('ишак ищет у тещи каши')) # True
